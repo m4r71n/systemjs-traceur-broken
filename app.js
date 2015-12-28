f4c4a842b85ae0f6477d0a7a0c1564a662e5b03d
Martin Lepadusch <martin.lepadusch@cit.de>
2015-12-28 09:38:07 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+let test = 'broken';
+
+console.log(test);
+
+export default test;
---
+++ config.js
@@ -0,0 +1,14 @@
+System.config({
+  baseURL: "/",
+  defaultJSExtensions: true,
+  transpiler: "traceur",
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
+  },
+
+  map: {
+    "traceur": "github:jmcriffey/bower-traceur@0.0.93",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.93"
+  }
+});
